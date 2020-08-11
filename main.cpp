0a9b1f694087085c6205442cc1afecfcf9a090d0
MarlonAdrian <66731201+MarlonAdrian@users.noreply.github.com>
2020-08-11 18:44:30 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,60 +1,44 @@
+
 #include <iostream>
-#include <fstream>
+
 using namespace std;
 
-#define tamanio(array) (sizeof(arrray)/sizeof(array[0]));
+int busqueda (int a[], int n, int dato);
 
 int main()
 {
-
-  ofstream archivoPrueba;
-  archivoPrueba.open ( "Binario.txt", ios::out);
-  int b,i,j,k;
-  int n;
-  int array[n];
-
-  cout << "Ingrese el tamaño del arreglo: ";
-  cin >> n;
-
-   archivoPrueba << "Arreglo: " ;
-    for(i=0; i<n; i++)
+    
+    
+    int a[5]={4,1,2,3,5};
+    
+    if(busqueda(a,5,5)==-1)
     {
-        cout << "Ingrese el elemento Nº" << i  << " del arreglo: ";
-        cin >> array[i];
-        archivoPrueba << array[i];
-    }  
-    archivoPrueba << endl;
-
-    cout << "Ingrese el elemento a buscar en el arreglo: ";
-    cin >> b;
-
-    archivoPrueba << "Elemento a buscar: " << b;
-    archivoPrueba << endl;
-    i=0;
-
-    j = array[i] -1;
- 
-    do{
-
-        k=(i+j)/2;
-
-        if (array[k]<=b)
-        {
-            i=k+1;
-        }
-
-
-        if(array[k]>=b)
-        {
-            j=k-1;
-        }
+        cout<<"NO EXISTE EL ELEMENTO";
     }
-    while( i <=j );
-
-    cout<<"El elemento encontrado es: "<< b << " y se encuentra en la posicion: "<< array[k];
-
-    archivoPrueba << "El elemento encontrado es: "<< b << " y se encuentra en la posicion: "<< array[k];
+    else
+    {
+        cout<<"ELEMENTO ESTA EN LA POSICIÓN: "<<busqueda(a,5,5);
+    }
+    
+ 
+    
 
     return 0;
+}
 
+
+int busqueda (int a[], int n, int dato)
+{
+    
+    for (int i=0; i<=n; i++)
+    {
+        if(a[i]==dato)
+        {
+            return i;
+        }
+    }
+  
+    
+        return -1;
+    
 }
